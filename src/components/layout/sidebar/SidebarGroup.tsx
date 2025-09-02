606603f8515ae75ff52c47d17774f8c666363493
Adrian Narendra <adrianenarendra@gmail.com>
2025-09-02 10:16:11 +0700

    added lucide-react and updated sidebar menu configuration

--- src/components/layout/sidebar/SidebarGroup.tsx
+++ src/components/layout/sidebar/SidebarGroup.tsx
@@ -1,54 +1,48 @@
 import Icon from '@/components/ui/users/icons'
 import { cn } from '@/utils/cn'
 import React, { useState } from 'react'
-import { MenuConfig, MenuPath } from './menuConfig'
 import { usePathname } from 'next/navigation'
 import SidebarItem from './SidebarItem'
+import {
+    ChevronDown
+} from "lucide-react";
 
-interface SidebarProps {
-    menu: MenuConfig
-}
 
-type dropdownType = 'open' | 'close';
-
-const SidebarGroup = ({ menu }: SidebarProps) => {
-    const [isOpen, setIsOpen] = useState<dropdownType>('close');
-
-    // const dropdownToggle = () => {
-    //     setDropdown([item.path] === 'arrowUp' ? true : false)
-    // }
+const SidebarGroup = ({ name, icon, path, children }) => {
+    const [isOpen, setIsOpen] = useState('close');
 
     return (
         <div className='flex flex-col gap-[8px] cursor-pointer'>
+            {/* Parent with submenu */}
             <div
                 onClick={() =>
-                    setIsOpen((prev) => prev === 'close' ? 'open' : 'close')
+                    name === 'Home' ? {} : setIsOpen((prev) => prev === 'close' ? 'open' : 'close')
                 }
                 className="text-primary mx-[20px] flex gap-[12px] p-[12px] text-[16px] font-semibold rounded-[8px]"
             >
                 <div>
-                    <Icon {...menu.icon} />
+                    {icon}
                 </div>
-                {menu.name}
-                {menu.name === 'Collapse'
+                {name}
+                {name === 'Home'
                     ? null
                     :
-                    <Icon
-                        name='arrowDown'
+                    <ChevronDown
                         className={cn(
                             "w-[24px] ml-auto stroke-primary chevron",
                             isOpen === 'open' && "rotate"
                         )}
                     />}
             </div>
-            {menu.path !== '/' &&
+            {path !== '/' &&
                 <div className={
                     cn(
                         "flex mx-[20px] gap-[8px]",
                         isOpen === 'open' ? 'block' : 'hidden'
                     )
                 }>
-                    {menu.children?.map((item) => (
+                    {/* Submenu */}
+                    {children?.map((item) => (
                         <SidebarItem
                             key={item.path}
                             name={item.name}
--- src/components/layout/sidebar/menuConfig.tsx
+++ src/components/layout/sidebar/menuConfig.tsx
@@ -1,100 +1,44 @@
-import { IconName } from "@/components/ui/users/icons";
-import path from "path";
+import {
+    BookOpen,
+    GraduationCap,
+    Wallet,
+    Home,
+} from "lucide-react";
 
-export interface IconConfig {
-    name: IconName,
-    className: string
-}
-
-export type MenuPath =
-    // TODO: if theres no default page, delete "/"
-    | "/"
-    | "/registration"
-    | "/education"
-    | "/finance"
-    ;
-
-interface ChildConfig {
-    name: string;
-    path: `${MenuPath}/${string}`
-}
-
-type MenuChildren = Record<MenuPath, ChildConfig[]>;
-
-export interface MenuConfig {
-    name: string,
-    path: MenuPath,
-    icon: IconConfig,
-    children?: ChildConfig[]
-}
-
-const menuChildren: MenuChildren = {
-    "/": [],
-    "/registration": [],
-    "/education": [],
-    "/finance": [
-        {
-            name: 'Dashboard',
-            path: '/finance/dashboard'
-        },
-        {
-            name: 'Cost Center',
-            path: '/finance/costcenter'
-        },
-        {
-            name: 'Daftar Ulang',
-            path: '/finance/reregister'
-        },
-        {
-            name: 'Biaya Operasional',
-            path: '/finance/operationalcost'
-        },
-        {
-            name: 'Biaya Pendidikan',
-            path: '/finance/educationcost'
-        },
-        {
-            name: 'Rekap Transaksi',
-            path: '/finance/transactionrecap'
-        },
-    ]
-}
-
-export const menus: MenuConfig[] = [
+const menu= [
     {
-        name: "Collapse",
-        path: "/",
-        icon: {
-            name: "collapse",
-            className: "w-[24px] stroke-primary"
-        },
-        children: menuChildren['/']
+        name: "Home",
+        icon: Home,
+        path: "/dashboard",
     },
     {
         name: "PMB",
-        path: "/registration",
-        icon: {
-            name: "pmb",
-            className: "w-[24px] stroke-primary"
-        },
-        children: menuChildren['/registration']
+        icon: BookOpen,
+        children: [
+            { name: "Pendaftaran", path: "/pmb/pendaftaran" },
+            { name: "Seleksi", path: "/pmb/seleksi" },
+        ],
     },
     {
         name: "KBM",
-        path: "/education",
-        icon: {
-            name: "kbm",
-            className: "w-[24px] stroke-primary"
-        },
-        children: menuChildren['/education']
+        icon: GraduationCap,
+        children: [
+            { name: "Jadwal", path: "/kbm/jadwal" },
+            { name: "Absensi", path: "/kbm/absensi" },
+        ],
     },
     {
         name: "Keuangan",
-        path: "/finance",
-        icon: {
-            name: "keuangan",
-            className: "w-[24px] stroke-primary"
-        },
-        children: menuChildren['/finance']
-    }
+        icon: Wallet,
+        children: [
+            { name: "Dashboard", path: "/keuangan" },
+            { name: "Cost Center", path: "/keuangan/cost-center" },
+            { name: "Daftar Ulang", path: "/keuangan/daftar-ulang" },
+            { name: "Biaya Operasional", path: "/keuangan/operasional" },
+            { name: "Biaya Pendidikan", path: "/keuangan/pendidikan" },
+            { name: "Rekap Transaksi", path: "/keuangan/rekap" },
+        ],
+    },
 ];
+
+export default menu
